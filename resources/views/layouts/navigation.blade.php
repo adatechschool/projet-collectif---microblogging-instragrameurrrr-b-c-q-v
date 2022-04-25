ad044560ed7f505a8c67d66793b1abbb774662d7
BaptisteGaubert <baptistegaubert18@gmail.com>
2022-04-25 11:23:09 +0200

    logo and co

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,14 +6,17 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <!-- <x-application-logo class="block h-10 w-auto fill-current text-gray-600" /> -->
+                        <!-- <img src="{{storage_path('app/logo_macaron.png')}}"></img> --> 
+                      
+                        <img src="hornithotrynque.png" class="block h-20 w-20 fill-current text-gray-600">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Hornithotrynque') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -64,7 +67,7 @@
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+                {{ __('Hornithotrynque') }}
             </x-responsive-nav-link>
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get("/profil", function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
